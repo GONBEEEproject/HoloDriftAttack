1f7bb03b677f9d5ff1aed393bb8088c9b1035012
GONBEEE_project_myhouse0 <gonbeee.project@gmail.com>
2019-02-04 01:48:06 +0900

    一通り組んだ、動作確認済

---
+++ Assets/Scripts/GazeGestureManager.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GazeGestureManager : MonoBehaviour
+{
+
+    private static GazeGestureManager instance;
+    public static GazeGestureManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GazeGestureManager>();
+            }
+            return instance;
+        }
+    }
+
+
+    // Represents the hologram that is currently being gazed at.
+    public GameObject FocusedObject { get; private set; }
+
+    GestureRecognizer recognizer;
+
+    public enum NowState
+    {
+        Start,Marker,TimeAttack
+    }
+
+    public NowState state = NowState.Start;
+
+    public GameObject StartFlag;
+    public GameObject Marker;
+
+    private GameObject FlagHolder;
+    private List<GameObject> MarkerHolder = new List<GameObject>();
+
+
+    
+    // Use this for initialization
+    void Start()
+    { 
+
+        // Set up a GestureRecognizer to detect Select gestures.
+        recognizer = new GestureRecognizer();
+        recognizer.Tapped += (args) =>
+        {
+            //// Send an OnSelect message to the focused object and its ancestors.
+            //if (FocusedObject != null)
+            //{
+            //    FocusedObject.SendMessageUpwards("OnSelect", SendMessageOptions.DontRequireReceiver);
+            //}
+
+            var headPosition = Camera.main.transform.position;
+            var gazeDirection = Camera.main.transform.forward;
+
+            RaycastHit info;
+
+            if (Physics.Raycast(headPosition, gazeDirection, out info))
+            {
+                Tap(info);
+            }
+
+
+
+
+        };
+        recognizer.StartCapturingGestures();
+    }
+
+    private void Tap(RaycastHit info)
+    {
+        switch (state)
+        {
+            case NowState.Start:
+
+                FlagHolder = Instantiate(StartFlag, info.point + new Vector3(0, 1.0f, 0), Quaternion.identity);
+                state = NowState.Marker;
+
+                MarkerManager.Instance.StartMarker(FlagHolder);
+
+                break;
+            case NowState.Marker:
+                if (info.transform.tag == StartFlag.tag)
+                {
+                    MarkerManager.Instance.EndMarker();
+
+                    state = NowState.TimeAttack;
+                }
+                else if (info.transform.tag == Marker.tag)
+                {
+                    break;
+                }
+                else
+                {
+                    GameObject tmp = Instantiate(Marker, info.point + new Vector3(0, 1.0f, 0), Quaternion.identity);
+
+                    MarkerManager.Instance.SetNewMarker(tmp.GetComponent<Node>());
+
+                }
+
+
+                break;
+            case NowState.TimeAttack:
+                Destroy(FlagHolder);
+                for(int i = 0; i < MarkerHolder.Count; i++)
+                {
+                    Destroy(MarkerHolder[i].gameObject);
+                }
+
+                state = NowState.Start;
+
+                break;
+        }
+    }
+
+
+
+
+    private void Update()
+    {
+
+        Vector2 touchScreenPosition = Input.mousePosition;
+
+        touchScreenPosition.x = Mathf.Clamp(touchScreenPosition.x, 0.0f, Screen.width);
+        touchScreenPosition.y = Mathf.Clamp(touchScreenPosition.y, 0.0f, Screen.height);
+
+        Camera gameCamera = Camera.main;
+        Ray touchPointToRay = gameCamera.ScreenPointToRay(touchScreenPosition);
+
+        // デバッグ機能を利用して、スクリーンビューでレイが出ているか見てみよう。
+        Debug.DrawRay(touchPointToRay.origin, touchPointToRay.direction * 1000.0f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit info;
+
+            if (Physics.Raycast(gameCamera.gameObject.transform.position, touchPointToRay.direction, out info))
+            {
+                Tap(info);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/MarkerManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerManager : MonoBehaviour
+{
+    private static MarkerManager instance;
+    public static MarkerManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<MarkerManager>();
+            }
+            return instance;
+        }
+    }
+
+
+    private List<Node> nodes = new List<Node>();
+    private int flagNum;
+
+    private TextMesh timeMesh;
+
+    private float scoreTime;
+
+    private bool isAttack=false;
+
+    public AudioClip StartSound, EndSound,NodeSound,NodePut;
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (isAttack)
+        {
+            scoreTime += Time.deltaTime;
+            timeMesh.text = scoreTime.ToString();
+        }
+
+    }
+
+
+
+    public void MarkerTouched(Node node)
+    {
+        if (nodes[0] == node)
+        {
+            nodes.Remove(node);
+            Destroy(node.gameObject);
+            source.PlayOneShot(NodeSound);
+        }
+    }
+
+    public void FlagTouched()
+    {
+        if (nodes.Count == 0)
+        {
+            isAttack = false;
+            source.PlayOneShot(EndSound);
+        }
+        else if (isAttack == false)
+        {
+            isAttack = true;
+            source.PlayOneShot(StartSound);
+        }
+    }
+
+    public void StartMarker(GameObject startFlag)
+    {
+        nodes = new List<Node>();
+        scoreTime = 0;
+
+        timeMesh = startFlag.GetComponentInChildren<TextMesh>();
+        source.PlayOneShot(NodePut);
+    }
+
+    public void SetNewMarker(Node newNode)
+    {
+        newNode.SetUp(nodes.Count + 1);
+        nodes.Add(newNode);
+        source.PlayOneShot(NodePut);
+    }
+
+    public void EndMarker()
+    {
+        source.PlayOneShot(EndSound);
+    }
+
+}
